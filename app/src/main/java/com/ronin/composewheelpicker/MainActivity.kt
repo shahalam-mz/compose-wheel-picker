54173adf19a1ab12c2b80925ada0ad2c1b01f245
Shah Alam <linktos.alam@gmail.com>
2024-12-09 19:44:15 +0500

    Initial Commit

---
+++ app/src/main/java/com/ronin/composewheelpicker/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.ronin.composewheelpicker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.ronin.composewheelpicker.ui.theme.ComposeWheelPickerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeWheelPickerTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text("Compose Wheel Picker")
+                }
+            }
+        }
+    }
+}
